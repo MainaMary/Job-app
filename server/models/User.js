4f859d5fa5ab378da7a68155c785a4404e85c6ad
Mary <marieshix98@gmail.com>
2022-12-21 21:45:31 +0300

    Install json webtoken

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const json = require("jsonwebtoken");
 // const validator = require("validator");
 const UserSchema = new mongoose.Schema({
   name: {
