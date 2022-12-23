238ad68cf188a942f2e594392c0cf13341bd3bc1
Mary <marieshix98@gmail.com>
2022-12-23 21:51:25 +0300

    Install react query for async state management

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Navbar from "./components/Navbar";
 import AppContextProvider from "./context/appContext";
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <AppContextProvider>
-    <BrowserRouter>
-      <Navbar />
-      <App />
-    </BrowserRouter>
-  </AppContextProvider>
+  <QueryClientProvider client={queryClient}>
+    <AppContextProvider>
+      <BrowserRouter>
+        <Navbar />
+        <App />
+      </BrowserRouter>
+    </AppContextProvider>
+  </QueryClientProvider>
 );
