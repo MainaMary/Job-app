ab4690674347b348d6a28b1ae7acccfaa3ca7ffe
Mary Maina <52330368+MainaMary@users.noreply.github.com>
2022-12-27 23:52:47 +0300

    Update CustomSpinner.tsx
    Update loader

--- frontend/src/components/CustomSpinner.tsx
+++ frontend/src/components/CustomSpinner.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CustomSpinner = () => {
-  return <div>Loading..</div>;
+  return <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>;
 };
 
 export default CustomSpinner;
