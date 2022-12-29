a42879c5bb7ef53b8abd6e6ae6eb03db610033ea
Mary <marieshix98@gmail.com>
2022-12-29 23:51:13 +0300

    Fix type error

--- frontend/src/components/CustomSpinner.tsx
+++ frontend/src/components/CustomSpinner.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const CustomSpinner = () => {
-  return <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>;
+  return (
+    <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
+  );
 };
 
 export default CustomSpinner;
