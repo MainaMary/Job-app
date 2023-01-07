0337c325c7ccf4f1305b8fb8a37e5dc7eb53623f
Mary <marieshix98@gmail.com>
2023-01-07 16:22:06 +0300

    Add fowardRef for the input component

--- frontend/src/components/CustomInput.tsx
+++ frontend/src/components/CustomInput.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef, HTMLInputTypeAttribute } from "react";
 interface InptProps {
   onChange?: (x: any) => void;
   type: string;
@@ -7,23 +7,21 @@ interface InptProps {
   value: string;
 }
 
-const CustomInput = ({
-  onChange,
-  type,
-  placeholder,
-  name,
-  value,
-}: InptProps) => {
-  return (
-    <input
-      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      onChange={onChange}
-      type={type}
-      placeholder={placeholder}
-      name={name}
-      value={value}
-    />
-  );
-};
+const CustomInput = forwardRef(
+  (props: InptProps, ref: HTMLInputTypeAttribute | any) => {
+    const { onChange, type, placeholder, name, value } = props;
+    return (
+      <input
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        onChange={onChange}
+        type={type}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        ref={ref}
+      />
+    );
+  }
+);
 
 export default CustomInput;
