5dba07a27aa4a429c9dfe911cb1c0bcb02ba7006
Mary <marieshix98@gmail.com>
2023-01-10 23:00:58 +0300

    Pass the onsuccess data to the context provider

--- frontend/src/context/appContext.tsx
+++ frontend/src/context/appContext.tsx
@@ -1,31 +1,37 @@
-import { any } from "joi";
+import { useState } from "react";
 import { useContext, useReducer } from "react";
 import { createContext } from "react";
 import { DISPLAY_ALERT, CLEAR_CART, REGISTER_USER_SUCCESS } from "./actons";
 import { reducer } from "./reducers";
 import { useRegisterUser } from "../hooks/usePost";
+import { useQueryClient } from "@tanstack/react-query";
 
 interface Props {
   children: JSX.Element;
 }
-export const initialValues = {
-  isLoading: true,
-  isError: false,
-  showAlert: false,
-  alertText: "",
-  alertType: "",
-  displayAlert: any,
-  clearAlert: any,
-  registerUser: any,
-};
-const appContext = createContext(initialValues);
+interface initialValues {
+  isLoading: true;
+  isError: false;
+  showAlert: false;
+  alertText: "";
+  alertType: "";
+  displayAlert: () => void;
+  clearAlert: () => void;
+  registerUser: (x: any) => void;
+  resData: [];
+  setResData: React.Dispatch<React.SetStateAction<never[]>>;
+}
+const appContext = createContext<initialValues>();
 
 export const useAppContext = () => {
   return useContext(appContext);
 };
 const AppContextProvider = ({ children }: Props) => {
   const [values, dispatch] = useReducer(reducer, initialValues);
+  const [resData, setResData] = useState([]);
+  const queryClient = useQueryClient();
   const { mutate } = useRegisterUser();
+  queryClient.setQueryData;
 
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT });
@@ -42,7 +48,14 @@ const AppContextProvider = ({ children }: Props) => {
 
   return (
     <appContext.Provider
-      value={{ ...values, displayAlert, clearAlert, registerUser }}
+      value={{
+        ...values,
+        displayAlert,
+        clearAlert,
+        registerUser,
+        resData,
+        setResData,
+      }}
     >
       {children}
     </appContext.Provider>
--- frontend/src/hooks/usePost.ts
+++ frontend/src/hooks/usePost.ts
@@ -1,16 +1,28 @@
 import { useMutation } from "@tanstack/react-query";
 import { baseUrl } from "../api/baseUrl";
 import axios from "axios";
-const registerUser = (payload: any) => {
-  console.log(baseUrl);
-  const response = axios.post(`${baseUrl}auth/register`, payload);
-  return response;
+import { useAppContext } from "../context/appContext";
+interface Props {
+  email: string;
+  password: string;
+  name: string;
+}
+const registerUser = async (payload: Props) => {
+  try {
+    console.log(baseUrl);
+    const response = await axios.post(`${baseUrl}auth/register`, payload);
+    return response;
+  } catch (error: any) {
+    return error.message;
+  }
 };
 
 export const useRegisterUser = () => {
+  const { setResData } = useAppContext();
   return useMutation({
     mutationFn: registerUser,
     onSuccess: (data) => {
+      setResData(data);
       console.log(data, "reponse data");
     },
   });
