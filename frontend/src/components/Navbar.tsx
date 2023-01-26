5f43d465d5e8838e0ba6c35dcf5b3d40c8139cf7
Mary <marieshix98@gmail.com>
2023-01-26 22:46:15 +0300

    Add functionality to handle nav responsiveness

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React,{useState} from "react";
 import Button from "./Button";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import {FaBars} from "react-icons/fa"
+import {VscChromeClose} from "react-icons/vsc"
 const Navbar = () => {
+  const [open, setOpen] = useState<boolean>(false)
   const navigate = useNavigate();
+  const handleBars = () =>{
+    setOpen(prev => !prev)
+  }
   return (
     <nav className="flex justify-between px-16 h-20 items-center ">
       <div
@@ -18,6 +24,9 @@ const Navbar = () => {
       <div className="w-[25%] flex justify-between">
         <Button name={<Link to="/login">Log in</Link>} />
         <Button name={<Link to="/register">Register</Link>} />
+      </div>
+      <div onClick={handleBars} className="block md:hidden">
+         {open ? <VscChromeClose/> : <FaBars/>}
       </div>
     </nav>
   );
