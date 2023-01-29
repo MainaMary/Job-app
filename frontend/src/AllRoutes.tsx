f5e3008ee51ffbcb7d7359f8f9085a7210d4cb32
Mary <marieshix98@gmail.com>
2023-01-29 23:49:17 +0300

    Setup nested routes

--- frontend/src/AllRoutes.tsx
+++ frontend/src/AllRoutes.tsx
@@ -3,10 +3,16 @@ import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
 import Register from "./pages/Register";
 import LogIn from "./pages/LogIn";
+import UserProfile from "./dashboard/UserProfile";
+import AddJob from "./dashboard/AddJob";
+import Alljobs from "./dashboard/Alljobs";
 const array = [
   { path: "/", component: <HomePage /> },
   { path: "/register", component: <Register /> },
   { path: "/LogIn", component: <LogIn /> },
+  {path: "/userProfile", component: <UserProfile /> },
+  {path:"/addJob", component:<AddJob/>},
+  {path:"/allJobs", component:<Alljobs/>},
   { path: "*", component: <NotFound /> },
 ];
 const AllRoutes = () => {
