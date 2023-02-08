067b3bc072347ed2b6b5ad0d7bf403e80b22188e
Mary <marieshix98@gmail.com>
2023-02-08 23:58:40 +0300

    Display a fallback while content is loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import AllRoutes from "./AllRoutes";
+import { Suspense } from "react";
+import CustomSpinner from "./components/CustomSpinner";
 
 function App() {
   return (
     <div className="px-8 md:px-16">
+      <Suspense fallback={<CustomSpinner/>}>
       <AllRoutes />
+      </Suspense>
+     
     </div>
   );
 }
