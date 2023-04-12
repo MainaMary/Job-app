5d5d299dc518ae38b71cea32d38f32300f3ff7c1
Mary <marieshix98@gmail.com>
2023-04-12 23:50:32 +0300

    Form input label

--- frontend/src/dashboard/AddJob.tsx
+++ frontend/src/dashboard/AddJob.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import CustomInput from '../components/CustomInput'
+import CustomLabel from '../components/CustomLabel'
 
 const AddJob = () => {
+  const handleSubmit = (e:any) =>{
+    e.preventDefault()
+  }
   return (
-    <div>AddJob</div>
+    <div>AddJob
+      <form onSubmit={handleSubmit}>
+        <div>
+          <CustomLabel>Job Title</CustomLabel>
+          <CustomInput type="text" value="job"/>
+        </div>
+        <div>
+          <CustomLabel>Company</CustomLabel>
+          <CustomInput type="text" value="company"/>
+        </div>
+      </form>
+    </div>
   )
 }
 
