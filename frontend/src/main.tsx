4e3c6b6fe28a357e6e0009935e2160d3483354ed
Mary <marieshix98@gmail.com>
2023-04-15 23:50:00 +0300

    Toaststify config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -13,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
     <AppContextProvider>
       <BrowserRouter>
+        <ToastContainer />
         <Navbar />
         <App />
       </BrowserRouter>
