821526d349ab66a772af6aaed15e88e6993c4626
Mary <marieshix98@gmail.com>
2023-04-20 23:52:18 +0300

    Define useDeete custom hook

---
+++ frontend/src/hooks/useDelete.ts
@@ -0,0 +1,26 @@
+import { useMutation } from "@tanstack/react-query";
+import { baseUrl } from "../api/baseUrl";
+import axios from "axios";
+import { useAppContext } from "../context/appContext";
+import { JobsContext } from "../types";
+interface Props {
+  email: string;
+  password: string;
+  name: string;
+}
+const registerUser = async (payload: Props) => {
+  const response = await axios.delete(`${baseUrl}auth/register`, payload);
+  return response.data;
+};
+
+export const useRegisterUser = () => {
+  const { setUser } = useAppContext();
+
+  return useMutation({
+    mutationFn: registerUser,
+    onSuccess: (data) => {
+      setUser(data);
+      console.log(data, "reponse data");
+    },
+  });
+};
