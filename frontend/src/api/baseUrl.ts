326ce37a96aec5cbd4b1814b9670e4aebcfee38b
Mary <marieshix98@gmail.com>
2023-04-30 23:50:07 +0300

    Define useFetch custom hook

--- frontend/src/api/baseUrl.ts
+++ frontend/src/api/baseUrl.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
-//export default axios.create({ baseURL: "http://localhost:8000/api/v1/" });
-export const baseUrl = "http://localhost:8000/api/v1/";
+
 
 
 const axiosInstance = axios.create({
