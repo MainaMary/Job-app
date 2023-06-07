93dbe5a0072c625d004d5a2d8101ad17759c0f2a
Mary <marieshix98@gmail.com>
2023-06-07 09:50:24 +0300

    update protected routes

--- frontend/src/routes/AllRoutes.tsx
+++ frontend/src/routes/AllRoutes.tsx
@@ -4,29 +4,37 @@ import NotFound from "../pages/NotFound";
 import AddJob from "../dashboard/AddJob";
 import Alljobs from "../dashboard/Alljobs";
 import Chat from "../pages/chat/Chat";
-const HomePage = React.lazy(() => import("../pages/HomePage"))
-const Register =React.lazy(()=> import("../pages/Register"))
-const UserProfile = React.lazy(() =>import( "../dashboard/UserProfile"))
-const LogIn = React.lazy(() =>import("../pages/LogIn"))
-const Home = React.lazy(() =>import("../pages/Dashboard"))
-const ProtectedRoute = React.lazy(() => import("../routes/ProtectedRoute"))
+const HomePage = React.lazy(() => import("../pages/HomePage"));
+const Register = React.lazy(() => import("../pages/Register"));
+const UserProfile = React.lazy(() => import("../dashboard/UserProfile"));
+const LogIn = React.lazy(() => import("../pages/LogIn"));
+const Home = React.lazy(() => import("../pages/Dashboard"));
+const ProtectedRoute = React.lazy(() => import("../routes/ProtectedRoute"));
 const array = [
-  { path: "/", component: <HomePage /> },
-  { path: "/register", component: <Register /> },
-  { path: "/LogIn", component: <LogIn /> },
-  {path: "/userProfile", component: <UserProfile /> },
-  {path:"/addJob", component:<AddJob/>},
-  {path:"/allJobs", component:<Alljobs/>},
-  {path:"/home", component:<Home/>},
-  {path:"/chat", component:<Chat/>},
-  { path: "*", component: <NotFound /> },
+  { path: "/", component: <HomePage />, isProtected: false },
+  { path: "/register", component: <Register />, isProtected: false },
+  { path: "/LogIn", component: <LogIn />, isProtected: false },
+  { path: "/userProfile", component: <UserProfile />, isProtected: false },
+  { path: "/addJob", component: <AddJob />, isProtected: false },
+  { path: "/allJobs", component: <Alljobs />, isProtected: false },
+  { path: "/home", component: <Home />, isProtected: false },
+  { path: "/chat", component: <Chat />, isProtected: false },
+  { path: "*", component: <NotFound />, isProtected: false },
 ];
 const AllRoutes = () => {
   return (
     <Routes>
-      {array.map(({ path, component }, index) => (
-        <Route key={index} path={path} element={component} />
-      ))}
+      {array.map(({ path, component, isProtected }, index) =>
+        isProtected ? (
+          <Route
+            path={path}
+            element={<ProtectedRoute>{component}</ProtectedRoute>}
+            key={index}
+          />
+        ) : (
+          <Route key={index} path={path} element={component} />
+        )
+      )}
     </Routes>
   );
 };
